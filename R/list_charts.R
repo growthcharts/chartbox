8550f6dbe084917add499ae4af90be7cad3639c2
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-11 22:51:45 +0100

    Add list_charts()

---
+++ R/list_charts.R
@@ -0,0 +1,35 @@
+#' List available growth charts
+#'
+#' @param chartgp The chart group
+#' @return A data frame
+#' @examples
+#' # list all available charts
+#' list_charts()
+#' @export
+list_charts <- function(chartgp = NULL) {
+
+  lib <- file.path(find.package("chartbox"), "library")
+  chartgps <- dir(lib)
+  if (!is.null(chartgp)) {
+    chartgps <- intersect(chartgp, chartgps)
+    if (length(chartgps) == 0L) {
+    warning("Chart group ", chartgp, " not found.")
+    return(NULL)
+    }
+  }
+
+  fn <- list.files(lib, recursive = TRUE)
+  f1 <- unlist(strsplit(unlist(strsplit(fn, split = "\\.")), "/"))
+  gp <- f1[seq(1, length(f1), 3)]
+  cd <- f1[seq(2, length(f1), 3)]
+  att <- t(sapply(cd, parse_chartcode))
+  att2 <- matrix(as.character(att), ncol = 6)
+  att3 <- data.frame(att2, stringsAsFactors = FALSE)
+  colnames(att3) <- dimnames(att)[[2]]
+  df <- data.frame(
+    chartgp = gp,
+    chartcode = cd,
+    att3,
+    stringsAsFactors = FALSE)
+  df
+}
