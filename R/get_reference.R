8b52e2dbe583b5be302db1e3b5074a483afd1ae7
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-14 09:04:40 +0100

    Add get_reference()

---
+++ R/get_reference.R
@@ -0,0 +1,27 @@
+#' Obtain the growth reference behind the chart
+#'
+#' @param chartgrp Chart group
+#' @param chartcode The chart code, returned by \code{chartbox::get_chartcode()}
+#' @param yname Names of the response variable
+#' @return An object of class \code{reference}
+#' @seealso \code{\link[clopus]{reference-class}}
+#' @examples
+#' \dontrun{
+#' get_reference("nl2010", "NJAA", "hgt")
+#' }
+#' @export
+get_reference <- function(chartgrp, chartcode, yname) {
+  tab <- chartbox::ynames_lookup
+  idx <- tab$chartgrp %in% chartgrp[1L] &
+    tab$chartcode %in% chartcode[1L] &
+    tab$yname %in% yname[1L]
+  if (sum(idx) > 1L) {
+    warning("Search combination not unique")
+    return(NULL)
+  }
+  if (sum(idx) < 1L) {
+    warning("Search combination not found")
+    return(NULL)
+  }
+  eval(parse(text = tab[idx, "reference"]))
+}
