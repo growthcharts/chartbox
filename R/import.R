49a2ed265d890c46c36609a320d79de3faf607db
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-15 14:56:49 +0100

    Add curve_interpolation()

--- R/import.R
+++ R/import.R
@@ -1,3 +1,9 @@
 #' @importFrom grDevices dev.off palette pdf
 #' @importFrom grid      grid.draw is.grob
+#' @importFrom rlang     .data :=
+#' @importFrom dplyr     bind_rows filter mutate %>% group_by
+#'                       n select as_tibble left_join distinct
+#'                       summarise ungroup arrange pull one_of
+#' @importFrom stats     approx
+#' @importFrom clopus    y2z z2y is.reference
 NULL
