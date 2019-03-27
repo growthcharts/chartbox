0ff913df199d819425295ae5bd3ca77787bb8217
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-03-27 09:53:03 +0100

    Add label expansion to set_form_design()

--- R/extract_rectangular.R
+++ R/extract_rectangular.R
@@ -6,6 +6,9 @@
 #' version preserve the original aspect ratio.
 #' @param chartcode The chart code
 #' @param ynames    Vector with names of measures to extract
+#' @param asp       Aspect ratio. Specify \code{asp = 1} for square
+#' data area. The default takes the aspect ratio from the
+#' chart.
 #' @param width     The width of the plot area + margins. The default
 #' is \code{unit("18", "cm")}
 #' @examples
@@ -15,11 +18,12 @@
 #' p <- extract_rectangular("NJAA")
 #' palette(palettes[1, ])
 #' grid.newpage()
-#' grid.draw(p[["wgt"]])
+#' grid.draw(p[["hgt"]])
 #' }
 #' @export
 extract_rectangular <- function(chartcode,
                                 ynames = c("hgt", "wgt", "hdc", "bmi", "wfh"),
+                                asp = NULL,
                                 width = unit(18, "cm")) {
   g <- load_chart(chartcode)
   ynames <- ynames[ynames %in% childNames(g)]
@@ -33,12 +37,17 @@ extract_rectangular <- function(chartcode,
     axes <- gy$children$axes
     form <- set_form_design(axes)
 
-    # define viewport and axes
+    # define aspect ratio
+    if (is.null(asp))
+    {
+      h <- g$childrenvp[[get_viewport(chartcode, yname)]]$height
+      w <- g$childrenvp[[get_viewport(chartcode, yname)]]$width
+      asp <- as.numeric(w) / as.numeric(h)
+    }
+
+    # define viewport
     xlim <- range(form$xya[, "x"])
     ylim <- range(form$xya[, "y"])
-    h <- g$childrenvp[[get_viewport(chartcode, yname)]]$height
-    w <- g$childrenvp[[get_viewport(chartcode, yname)]]$width
-    asp <- as.numeric(w) / as.numeric(h)
     vp <- chaco::viewport.scatterplot(x = xlim, y = ylim, f = 0,
                                       width = width, asp = asp)
 
