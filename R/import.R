1f3e7ed08486f4ff459cbdd54984fdf6d0489216
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-05 15:52:07 +0200

    Take over set_curves() and friends from groeivoorspeller package

--- R/import.R
+++ R/import.R
@@ -1,10 +1,16 @@
-#' @importFrom grDevices dev.off palette pdf
-#' @importFrom grid      grid.draw is.grob unit childNames gList
-#'                       gTree setGrob
-#' @importFrom rlang     .data :=
-#' @importFrom dplyr     bind_rows filter mutate %>% group_by
-#'                       n select as_tibble left_join distinct
-#'                       summarise ungroup arrange pull one_of
-#' @importFrom stats     approx
-#' @importFrom clopus    y2z z2y is.reference
+#' @import donordata
+#' @importFrom grDevices    dev.off palette pdf
+#' @importFrom grid         grid.draw is.grob unit childNames gList
+#'                          gTree setGrob polylineGrob gpar
+#'                          pointsGrob
+#'                          clipGrob gPath editGrob
+#' @importFrom rlang        .data :=
+#' @importFrom dplyr        bind_rows filter mutate %>% group_by
+#'                          n select as_tibble left_join distinct
+#'                          summarise ungroup arrange pull one_of
+#' @importFrom stats        approx na.omit
+#' @importFrom minihealth   is.individual get_xyz
+#' @importFrom clopus       y2z z2y is.reference
+#' @importFrom donorloader  load_child_data load_time_data
+#' @importFrom chaco        gridlinesGrob axesGrob get.form.design
 NULL
