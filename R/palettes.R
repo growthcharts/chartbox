d150a341546bbea590edd4af4d6a7cb22d2f33bb
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-08-05 16:09:08 +0200

    Improve documentation

--- R/palettes.R
+++ R/palettes.R
@@ -4,12 +4,13 @@
 #' @docType data
 #' @format A data frame with 10 observations 6 variables
 #' \describe{
-#' \item{bg}{Background color}
-#' \item{fg}{Foreground color}
-#' \item{dark}{Blackish color}
-#' \item{light}{Lightish color}
-#' \item{aux1}{Color aux1}
-#' \item{aux2}{Color aux2} }
+#' \item{bg}{Background color: Always \code{#F7F7F7} (\code{grey97})}
+#' \item{fg}{Foreground color. The major chart color that
+#' indicates the population. Used for lines and smaller fills.}
+#' \item{dark}{Blackish color for text and labels.}
+#' \item{light}{Light color used for filling the normal centile range.}
+#' \item{aux1}{Color aux1.}
+#' \item{aux2}{Color aux2}}
 #' @seealso \code{\link[chartdesigner]{create_palettes}}
 #' @keywords datasets
 NULL
