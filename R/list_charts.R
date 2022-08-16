7ab36ea7669e24ee7977ca4eafac11f939b52b1e
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-08-16 15:56:55 +0200

    Expand chartgrp documentation

--- R/list_charts.R
+++ R/list_charts.R
@@ -1,7 +1,22 @@
 #' List available growth charts
 #'
-#' @param chartgrp The chart group
-#' @return A data frame
+#' @param chartgrp Optional. String chart group name, e.g. \code{chartgrp = "who"}.
+#' If omitted, \code{list_charts()} return all charts groups.
+#' @return A \code{data.frame} with eight columns:
+#' \describe{
+#' \item{\code{chartgrp}}{Chart group code}
+#' \item{\code{chartcode}}{Unique chart code}
+#' \item{\code{population}}{Population code: DS (Down Syndrome), HS (Hindostanic),
+#' MA (Morrocan), NL (Dutch), PT (Preterm), TU (Turkish), WHOblue (WHO male),
+#' WHOpink (WHO female)}
+#' \item{\code{sex}}{Either \code{"male"}  or \code{"female"}}
+#' \item{\code{design}}{Chart design A: 0-15m, B: 0-4y (WFH), C: 1-21y, D: 0-21y, E: 0-4y (WFA)}
+#' \item{\code{side}}{Outcome measure: hgt (height), wgt (weight), hdc (head circumference),
+#' wfh (weight for height), bmi (body mass index), front (multiple), back (multiple),
+#' -hdc (back, no head circumference)}
+#' \item{\code{language}}{Chart language: dutch}
+#' \item{\code{week}}{Weeks of gestation 25-36, or missing (>= 37 weeks)}
+#' }
 #' @examples
 #' # list all available charts
 #' list_charts()
